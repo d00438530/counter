e012700c35188d5b2f9efa5d25734009ff6f9fde
d00438530 <103125971+d00438530@users.noreply.github.com>
2022-04-08 08:16:31 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    while (count < 10) {
+        basic.pause(200)
+        basic.showNumber(count)
+        count += 1
+    }
+})
+let count = 0
+count = 0
